30509518ccb9390431ce123c982a88863066a219
to4to <justjustifying@outlook.com>
2023-11-23 03:58:58 +0530

    main complite starting query

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"net/http"
 	"os"
+
+	"strings"
 )
 
 type apiConfigData struct {
@@ -35,7 +38,31 @@ func loadApiConfig(filename string) (apiConfigData, error) {
 	}
 
 	return c, nil
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello from go!\n"))
+}
+
+func query(city string) (weatherData, error) {
+
+	
 
 }
 
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		city := strings.SplitN(r.URL.Path, "/", 3)[2]
+		data, err := query(city)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("ontent-Type", "application/json;charset=utf-8")
+		json.NewEncoder(w).Encode(data)
 
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
