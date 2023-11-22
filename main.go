da03fa0fc0d6c00347bf47c76759322fb356c3da
to4to <justjustifying@outlook.com>
2023-11-23 03:07:27 +0530

    structs of apiconfig + weatherData

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+
+
+
+
+type apiConfigData struct{
+
+
+	OpenWeatherApiKey string `json:"OpenWeatherMapApiKey"`
+}
+
+type weatherData struct{
+
+	Name string `json:"name"`
+
+	Main struct{
+		Kelvin float64 `json:"temp"`
+	}`json:"main"`
+}
